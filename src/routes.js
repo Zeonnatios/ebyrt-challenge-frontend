20b30169d4146d61dee8ea181265121fba4a895c
Matheus Antonio <matheusantonio232@gmail.com>
2021-11-02 17:06:07 -0300

    feat(routes): adicionado rota de login

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+
+function routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default routes;
