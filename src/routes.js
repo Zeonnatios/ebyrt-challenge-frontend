a0a7bc4be63abb85643fb25159eedc3439b5f78c
Matheus Antonio <matheusantonio232@gmail.com>
2021-11-02 18:29:24 -0300

    refactor: removendo redux do projeto

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
+import TodoList from './pages/TodoList';
 
 function routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/todoList" component={ TodoList } />
       </Switch>
     </BrowserRouter>
   );
