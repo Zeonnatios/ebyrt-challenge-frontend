163041ba0675c65614436639cc7a5065b954365b
Matheus Antonio <matheusantonio232@gmail.com>
2021-11-02 22:08:07 -0300

    feat: finalizado template task/header.js

--- src/components/Tasks/Header.js
+++ src/components/Tasks/Header.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FaUserCircle, FaSignOutAlt } from 'react-icons/fa';
 
 function Header() {
   return (
@@ -6,7 +9,13 @@ function Header() {
       <h1>Lista de Tarefas</h1>
       <div className="user-info">
         <div className="user-info-container">
-          menu
+          <FaUserCircle className="header-icon" size="32" />
+          <span>userName</span>
+        </div>
+        <div className="user-info-container">
+          <Link to="/">
+            <FaSignOutAlt className="header-icon header-icon-logout" size="32" />
+          </Link>
         </div>
       </div>
     </header>
